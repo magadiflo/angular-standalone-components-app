22bf242e071214288180a25c298c6acc29333623
Martín <magadiflo@gmail.com>
2023-01-25 20:32:04 -0500

    Usando un standalone component dentro de otro standalon component

--- src/app/pages/dogs-list/dogs-list.component.ts
+++ src/app/pages/dogs-list/dogs-list.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { DogsService } from '../../services/dogs.service';
 import { Dog } from '../../interfaces/dog.interface';
 
+import { DogsCardComponent } from '../dogs-card/dogs-card.component';
+
 /**
  * * Standalone Component
  * * ********************
@@ -13,6 +15,8 @@ import { Dog } from '../../interfaces/dog.interface';
  * * se le coloca el atributo standalone en true, haciéndolo un componente Standalone, es decir
  * * este componente será a la vez su propio módulo, en el que además se puede agregar los atributos
  * * propios de un módulo como el imports, etc.
+ * *
+ * * Los standalone components gestionan explícitamente sus propias dependencias
  */
 
 @Component({
@@ -20,7 +24,7 @@ import { Dog } from '../../interfaces/dog.interface';
   templateUrl: './dogs-list.component.html',
   styleUrls: ['./dogs-list.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DogsCardComponent],
 })
 export class DogsListComponent implements OnInit {
 
