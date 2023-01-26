e027e3b8b959a0d7ef9e923f92467b5c67913f73
Martín <magadiflo@gmail.com>
2023-01-25 20:13:06 -0500

    Creación del primer componente Standalone dogsList

---
+++ src/app/pages/dogs-list/dogs-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DogsService } from '../../services/dogs.service';
+import { Dog } from '../../interfaces/dog.interface';
+
+/**
+ * * Standalone Component
+ * * ********************
+ * * Este componente no es un componente cualquiera, sino que es un standalone component,
+ * * eso significa que este componente no necesita ser declarado en el declarations: []
+ * * de algún módulo, tal como lo haría un componente normal. Esto es porque este componente,
+ * * se le coloca el atributo standalone en true, haciéndolo un componente Standalone, es decir
+ * * este componente será a la vez su propio módulo, en el que además se puede agregar los atributos
+ * * propios de un módulo como el imports, etc.
+ */
+
+@Component({
+  selector: 'app-dogs-list',
+  templateUrl: './dogs-list.component.html',
+  styleUrls: ['./dogs-list.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class DogsListComponent implements OnInit {
+
+  dogs: Dog[] = [];
+
+  constructor(private readonly _dogService: DogsService) { }
+
+  ngOnInit(): void {
+    this.dogs = this._dogService.dogs;
+  }
+
+}
