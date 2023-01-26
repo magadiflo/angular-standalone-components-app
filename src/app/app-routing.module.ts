22716249ea6e82adaff90b279cb860fb73899e8a
Martín <magadiflo@gmail.com>
2023-01-25 22:18:03 -0500

    Lazy load al Standalone component DogViewComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DogsListComponent } from './pages/dogs-list/dogs-list.component';
-import { DogViewComponent } from './pages/dog-view/dog-view.component';
+
+/**
+ * * Lazy load a un componente Standalone
+ * * ************************************
+ * * La nueva funcionalidad en el Router de Angular permite la 
+ * * carga diferida (Lazy load) y la división de código en un 
+ * * nivel de componente. 
+ * *
+ * * En este ejemplo, se hace uso del Lazy load aplicado
+ * * al componente Standalone DogViewComponent.
+ */
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'list' },
   { path: 'list', component: DogsListComponent },
-  { path: 'details/:index', component: DogViewComponent },
+  {
+    path: 'details/:index',
+    loadComponent: () => import('./pages/dog-view/dog-view.component').then(m => m.DogViewComponent)
+  },
 ];
 
 @NgModule({
